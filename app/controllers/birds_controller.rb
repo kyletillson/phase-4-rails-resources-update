21a3c44547528f25b828b0e1d34421bbf223a489
Kyle Tillson <kyletillson004@gmail.com>
2022-09-19 11:28:10 -0600

    commit tree b80dbb5027c75cbf4b6b14e31f9d4a1f688c7ec6

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -28,4 +28,24 @@ class BirdsController < ApplicationController
     params.permit(:name, :species)
   end
 
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
 end
